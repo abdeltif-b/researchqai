87b5fbefdcb7ef7af56006aa76dd860b92573754
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-07 17:15:48 +0100

    - create dashboard page - add stripe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import { cn, constructMetadata } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
-
+import "react-loading-skeleton/dist/skeleton.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = constructMetadata();

78c7358d682264499df179b5c216791d5d4bf1de
Abdeltif.B <Abdeltif.B@gclgroup.com>
2023-10-10 04:07:35 +0100

    improve page styling and add images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,58 @@
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn, constructMetadata } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 import "react-loading-skeleton/dist/skeleton.css";
 import "simplebar-react/dist/simplebar.min.css";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = constructMetadata();
 
+const Background = () => (
+  <div className="absolute inset-0 -z-10 overflow-hidden">
+    <svg
+      className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+      aria-hidden="true"
+    >
+      <defs>
+        <pattern
+          id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+          width={200}
+          height={200}
+          x="50%"
+          y={-1}
+          patternUnits="userSpaceOnUse"
+        >
+          <path d="M100 200V.5M.5 .5H200" fill="none" />
+        </pattern>
+      </defs>
+      <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+        <path
+          d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+          strokeWidth={0}
+        />
+      </svg>
+      <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
+    </svg>
+  </div>
+);
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <Providers>
-        <body className={cn("bg-gradient-to-tr from-white to-gray-100", inter.className)}>
+        <body className={cn("bg-gradient-to-tr from-white to-gray-100 min-h-screen", inter.className)}>
           <Toaster />
           <Navbar />
+          <Background />
+
           {children}
+
+          <Footer />
         </body>
       </Providers>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,126 +1,164 @@
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, GraduationCapIcon, LanguagesIcon, MicroscopeIcon } from "lucide-react";
 import { buttonVariants } from "@/components/ui/button";
 import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { ReactNode } from "react";
+
+const StepUI: React.FC<{ step: number; title: string; children: ReactNode }> = ({ step, title, children }) => (
+  <div className="sm:text-center">
+    <div className="flex items-center justify-center w-24 h-24 mb-4 text-5xl font-extrabold rounded-full text-blue-400 bg-blue-100 sm:mx-auto">
+      {step}
+    </div>
+    <h6 className="mb-2 text-xl font-semibold leading-5">{title}</h6>
+    <p className="sm:mx-auto max-w-md mb-3 mt-2 text-zinc-700">{children}</p>
+  </div>
+);
+
+const FeatureUI: React.FC<{ icon: ReactNode; title: string; description: string }> = ({ icon, title, description }) => (
+  <div className="p-8 border-t sm:border-r">
+    <div className="max-w-md text-center">
+      <div className="flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-blue-100 text-blue-400 sm:w-16 sm:h-16">
+        {icon}
+      </div>
+      <h6 className="mb-2 text-xl font-semibold leading-5">{title}</h6>
+      <p className="mb-3 text-zinc-700">{description} </p>
+    </div>
+  </div>
+);
 
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
-        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-          <p className="text-sm font-semibold text-gray-700">30% off for a limited time!</p>
-        </div>
-        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Simplify Research with <span className="text-blue-600">ResearchQAI</span>.
-        </h1>
-        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Revolutionize the Way You Interact with Research Papers
-        </p>
+      <MaxWidthWrapper className="flex flex-col lg:flex-col mb-12 mt-6 items-center justify-center text-center">
+        <div>
+          <h1 className="mt-20 max-w-4xl text-5xl font-bold">Chat with any research paper</h1>
 
-        <Link
-          className={buttonVariants({
-            className: "mt-5",
-          })}
-          href="/dashboard"
-          target="_blank"
-        >
-          Get started <ArrowRight className="ml-2 h-5 w-5" />
-        </Link>
-      </MaxWidthWrapper>
+          <p className="mt-5 max-w-prose text-zinc-700 sm:text-3xl">Ask questions about any research paper using AI.</p>
+          <p className="mt-5 max-w-prose text-zinc-900 sm:text-lg">
+            We make research papers easier to understand by providing summaries, answers to questions, and information
+            retrieval.
+          </p>
 
-      {/* value proposition section */}
-      <div>
-        <div className="relative isolate">
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          <Link
+            className={buttonVariants({
+              className: "mt-5",
+            })}
+            href="/dashboard"
           >
-            <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/3 rotate-[30deg] bg-gradient-to-tr from-blue-100 to-blue-600 opacity-10 sm:left-[calc(50%-30rem)] sm:w-[100.1875rem]" />
-          </div>
+            Get started <ArrowRight className="ml-2 h-5 w-5" />
+          </Link>
+        </div>
 
-          <div>
-            <div className="mx-auto max-w-6xl px-6 lg:px-8">
-              <div className="mt-16 flow-root sm:mt-24">
-                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                  <Image
-                    src="/image1.jpg"
-                    alt="image1"
-                    width={1364}
-                    height={866}
-                    quality={100}
-                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
-                  />
+        <div>
+          <div className="relative isolate">
+            <div
+              aria-hidden="true"
+              className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            >
+              <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/3 rotate-[30deg] bg-gradient-to-tr from-blue-100 to-blue-600 opacity-10 sm:left-[calc(50%-30rem)] sm:w-[100.1875rem]" />
+            </div>
+
+            <div>
+              <div className="mx-auto max-w-6xl px-6 lg:px-8">
+                <div className="mt-16 flow-root sm:mt-24">
+                  <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                    <Image
+                      src="/img/screenshot_1.png"
+                      alt="screenshot_1"
+                      width={1904}
+                      height={934}
+                      quality={100}
+                      className="rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+
+            <div
+              aria-hidden="true"
+              className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            ></div>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+
+      {/* Feature section */}
+      <div className="relative  py-1">
+        <div className="mx-auto mb-10 mt-20 max-w-5xl">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="mt-2 font-bold text-3xl text-gray-900">Start chatting in minutes</h2>
+              <p className="mt-4 text-lg text-gray-600">
+                Unlock the Power of AI to Interact with scientific papers and academic articles.
+              </p>
+            </div>
           </div>
 
-          <div
-            aria-hidden="true"
-            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          ></div>
+          {/* steps */}
+          <div className="grid gap-8 row-gap-8 lg:grid-cols-3">
+            <StepUI step={1} title={"Sign up for an account"}>
+              Either starting out with a free plan or choose our{" "}
+              <Link href="/pricing" className="text-blue-700 underline underline-offset-2">
+                pro plan.
+              </Link>
+            </StepUI>
+            <StepUI step={2} title={"Upload or link a research paper"}>
+              We&apos;ll process the research paper and make it ready for you to chat with.
+            </StepUI>
+            <StepUI step={3} title={"Engage in Conversation with AI"}>
+              Ask questions or request summaries. It&apos;s that simple. Try out researchQAI today for free.
+            </StepUI>
+          </div>
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="mt-16 flow-root sm:mt-24">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                <Image
+                  src="/img/screenshot_2.png"
+                  alt="screenshot_2"
+                  width={1919}
+                  height={935}
+                  quality={100}
+                  className="rounded-md bg-white shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      {/* Feature section */}
-      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
-        <div className="mb-12 px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl sm:text-center">
-            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">Start chatting in minutes</h2>
-            <p className="mt-4 text-lg text-gray-600">Unlock the Power of AI to Interact with Research Papers</p>
-          </div>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="grid row-gap-8 sm:grid-cols-1 lg:grid-cols-3">
+          <FeatureUI
+            icon={<MicroscopeIcon />}
+            title={"For Researchers"}
+            description={
+              "Access the information necessary for your research from scientific papers, scholarly articles, and books."
+            }
+          />
+          <FeatureUI
+            icon={<GraduationCapIcon />}
+            title={"For Students"}
+            description={"Get ready for exams and get help with assignments."}
+          />
+          <FeatureUI
+            icon={<LanguagesIcon />}
+            title={"Any Language"}
+            description={
+              "Functions globally! researchqai can handle PDFs in any language and engage in conversations in any language."
+            }
+          />
         </div>
-
-        {/* steps */}
-        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-              <span className="text-sm font-medium text-blue-600">Step 1</span>
-              <span className="text-xl font-semibold">Sign up for an account</span>
-              <span className="mt-2 text-zinc-700">
-                Either starting out with a free plan or choose our{" "}
-                <Link href="/pricing" className="text-blue-700 underline underline-offset-2">
-                  pro plan
-                </Link>
-                .
-              </span>
-            </div>
-          </li>
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-              <span className="text-sm font-medium text-blue-600">Step 2</span>
-              <span className="text-xl font-semibold">Upload or link a research paper</span>
-              <span className="mt-2 text-zinc-700">
-                We&apos;ll process the research paper and make it ready for you to chat with.
-              </span>
-            </div>
-          </li>
-          <li className="md:flex-1">
-            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
-              <span className="text-sm font-medium text-blue-600">Step 3</span>
-              <span className="text-xl font-semibold">Engage in Conversation with AI</span>
-              <span className="mt-2 text-zinc-700">
-                Ask questions or request summaries. It&apos;s that simple. Try out researchQAI today for free.
-              </span>
-            </div>
-          </li>
-        </ol>
-
-        <div className="mx-auto max-w-6xl px-6 lg:px-8">
-          <div className="mt-16 flow-root sm:mt-24">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <Image
-                src="/image2.jpg"
-                alt="image2"
-                width={1419}
-                height={732}
-                quality={100}
-                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
-              />
-            </div>
-          </div>
-        </div>
+        <Link
+          className={buttonVariants({
+            className: "w-full",
+            variant: "outline",
+          })}
+          href="/dashboard"
+        >
+          Get started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
       </div>
     </>
   );
